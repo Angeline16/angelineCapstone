f047a1ae1f80e80685ae71bed1901c89bff6f978
Your Name <deudajm@gmail.com>
2024-03-26 08:52:14 +0800

    add chat messages main page

--- views/ChatMainPage.php
+++ views/ChatMainPage.php
@@ -1,39 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://code.iconify.design/iconify-icon/2.0.0/iconify-icon.min.js"></script>
     <style>
-      @import url("https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap");
+        @import url("https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap");
 
-      body {
-        font-family: "Nunito", sans-serif;
-      }
+        body {
+            font-family: "Nunito", sans-serif;
+        }
 
-      .trade {
-        color: #0e7490;
-        font-weight: bold;
-      }
+        .chat {
+            color: #0e7490;
+            font-weight: bold;
+        }
     </style>
-  </head>
+</head>
 
-  <body>
+<body>
     <?php include '../components/header.php'; ?>
     <?php include '../components/sidebar.php'; ?>
     <div class="m-3 sm:pl-60 text-gray-800">
-      <div class="">
-        <h1 class="text-3xl font-extrabold">Messages</h1>
-      </div>
-      <div>
-        <div class="bg-cyan-400/10 p-2 my-5 text-cyan-600 rounded-md">
-          No messages yet!
+        <div class="">
+            <h1 class="text-3xl font-extrabold">Messages</h1>
         </div>
-      </div>
+        <div>
+            <div class="bg-cyan-400/10 p-2 my-5 text-cyan-600 rounded-md">
+                No messages yet!
+            </div>
+        </div>
     </div>
 
     <script src="../scripts/scripts.js"></script>
-  </body>
+</body>
+
 </html>
